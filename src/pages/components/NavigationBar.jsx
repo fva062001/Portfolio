fae840e122b27908895f8a3bfcd9d4bab3262258
Fernando Arturo Valerio <fernandoarturo06@gmail.com>
2023-03-10 18:28:33 -0400

    Hotfix

--- src/pages/components/NavigationBar.jsx
+++ src/pages/components/NavigationBar.jsx
@@ -3,7 +3,7 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function NavigationBar() {
   return (
-    <div className="w-[85vw] mt-4 lg:w-[94w] absolute flex flex-row mx-auto lg:mt-8 items-center text-white justify-between left-1/2 -translate-x-1/2 transform">
+    <div className="w-11/12 mt-4  absolute flex flex-row mx-auto lg:mt-8 items-center text-white justify-between left-1/2 -translate-x-1/2 transform">
       <h1 className="font-bold text-6xl ">FV</h1>
       <FontAwesomeIcon
         className="sm:inline-block md:inline-block lg:hidden xl:hiddden 2xl:hidden text-2xl px-2 py-2 border-[1.5px] border-white rounded-lg"
