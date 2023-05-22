2791d178b8094661bb2b62915d32b74a0f71c9c7
Fernando Arturo Valerio <fernandoarturo06@gmail.com>
2023-05-22 10:01:50 -0400

    Hotfix google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
 import "@/styles/global.css";
+import Script from "next/script";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}) {
+  return (
+    <>
+      <Script
+        strategy="lazyOnLoad"
+        src={
+          "https://www.googletagmanager.com/gtag/js?id=G-ZE8HG6L9N4"
+        }></Script>
+      <Script strategy="lazyOnLoad">{`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZE8HG6L9N4');`}</Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
