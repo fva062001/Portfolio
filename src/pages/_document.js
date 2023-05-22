d467fa92c8bc6a8b9c2ff9d84a3ed14c021a7c87
Fernando Arturo Valerio <fernandoarturo06@gmail.com>
2023-05-22 09:52:29 -0400

    Adding Google Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,13 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZE8HG6L9N4"></script>
+      <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-ZE8HG6L9N4');
+      </script>
     </Html>
   );
 }
