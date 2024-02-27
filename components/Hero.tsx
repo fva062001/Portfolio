8abf58555c1ac63b83ca51bc70ade4a352261799
Fernando Arturo Valerio <fernando.valerio@ambiente.gob.do>
2024-02-27 18:48:54 -0400

    Adding hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,24 @@
+'use client';
+import React from 'react';
+import { WavyBackground } from './wavy-background';
+
+function Hero() {
+  return (
+    <WavyBackground
+      speed="fast"
+      waveOpacity={0.5}
+      waveWidth={100}
+      className="max-w-4xl mx-auto pb-40">
+      <div className="flex flex-col items-center justify-center h-full text-center">
+        <p className="text-2xl md:text-4xl lg:text-7xl text-white font-extrabold inter-var">
+          Fernando Arturo Valerio
+        </p>
+        <p className="text-base md:text-lg mt-4 text-white font-normal inter-var">
+          Software Engineer
+        </p>
+      </div>
+    </WavyBackground>
+  );
+}
+
+export default Hero;
